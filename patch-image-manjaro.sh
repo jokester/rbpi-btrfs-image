7f1370bee6926695a16677d79525215680ab7526
Wang Guan <momocraft@gmail.com>
2021-02-06 23:17:44 +0900

    coerce partition in manjaro image

--- patch-image-manjaro.sh
+++ patch-image-manjaro.sh
@@ -5,7 +5,7 @@ set -ue
 if [[ $# -ne 2 ]]; then
   cat <<END
   USAGE: $0 DISK_IMAGE LOOP_DEVICE
-  example: $0 /dev/loop123 20201112_raspi_4.img
+  example: $0 /dev/loop123 Manjaro-ARM-minimal-rpi4-20.12.1.img
 END
   exit 1
 fi
@@ -13,14 +13,10 @@ fi
 cd $(dirname "$0")
 source ./functions
 
-prepare-mountpoints
-
 set -x
+
+truncate --size=3G "$2"
 
 map-image "$1" "$2"
 
-convert-partition "$1"
-
-debian-patch-boot "$1"
-
-cleanup "$1"
+resize-partition-2 "$1"
